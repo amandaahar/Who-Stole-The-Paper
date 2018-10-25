5576c1a93214a922b229d0f0d00d81d0a73ffce3
Amanda Harris <amanda.harris12h@gmail.com>
2018-10-25 12:37:00 -0400

    Update index-app.ts

--- index-app.ts
+++ index-app.ts
@@ -1,10 +1,6 @@
 /* 
 * Author: Amanda Harris
-* ONYEN: amandaar
-* UNC Honor Pledge: I certify that no unauthorized assistance has been received 
-* or given in the completion of this work. I certify that I understand and 
-* could now rewrite on my own, without assistance from course staff, 
-* the problem set code I am submitting. 
+*
 */
 
 import { print, csvToArray } from "introcs";
